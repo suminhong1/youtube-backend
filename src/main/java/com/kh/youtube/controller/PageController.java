7f366295b686938b03c6c0c4859c3e698dd78154
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-09-07 18:09:53 +0900

    2023-09-07

---
+++ src/main/java/com/kh/youtube/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.kh.youtube.controller;
+
+import com.kh.youtube.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @Autowired
+    private CategoryService category;
+
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("category", category.showAll());
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/kh/youtube/domain/Category.java
@@ -0,0 +1,22 @@
+package com.kh.youtube.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @Column(name="category_code")
+    @GeneratedValue(strategy= GenerationType.IDENTITY, generator = "categorySequence")//generator name이랑 맞추기
+    @SequenceGenerator(name = "categorySequence", sequenceName = "SEQ_CATEGORY", allocationSize = 1)
+    private int categoryCode;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+}
