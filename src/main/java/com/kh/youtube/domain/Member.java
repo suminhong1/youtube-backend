1d4471c7b5556594863a5ee9899615d91a223566
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-09-08 17:48:08 +0900

    2023-09-08

--- src/main/java/com/kh/youtube/domain/Member.java
+++ src/main/java/com/kh/youtube/domain/Member.java
@@ -1,21 +1,31 @@
 package com.kh.youtube.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert
 public class Member {
 
-    private String memberId;
-    private String memberPassword;
-    private String memberNickname;
-    private String memberEmail;
-    private String memberPhone;
-    private char memberGender;
-    private String memberAuthority;
+    @Id
+    private String id;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String authority;
 
 
 }
--- src/main/java/com/kh/youtube/repo/ChannelDAO.java
+++ src/main/java/com/kh/youtube/repo/ChannelDAO.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ChannelDAO extends JpaRepository<Channel, Integer> {
-
+    
     // 특정 멤버의 모든 채널 조회
-    // SELECT * FROM channel WHERE member_id=
-    @Query(value = "SELECT * FROM channel WHERE member_id= :memberId", nativeQuery = true)
-    List<Channel> findByMemberId(String memberId);
+    // SELECT * FROM channel WHERE id=?
+    @Query(value = "SELECT * FROM channel WHERE id= :id", nativeQuery = true)
+    List<Channel> findByMemberId(String id);
 }
