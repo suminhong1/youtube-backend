6d681d3af277d5bc42f9c69541a5190e17a5c78a
suminhong1 <125255022+suminhong1@users.noreply.github.com>
2023-09-21 19:23:23 +0900

    2023-09-21

--- src/main/java/com/kh/youtube/controller/ChannelController.java
+++ src/main/java/com/kh/youtube/controller/ChannelController.java
@@ -74,7 +74,7 @@ public class ChannelController {
 
         // 파일 업로드가 끝났으니 경로 (savePhoto), name, desc, memberId(id)
         Channel vo = new Channel();
-        vo.setChannelPhoto(savePhoto);
+        vo.setChannelPhoto(uuid + "_" + filePhoto);
         vo.setChannelName(name);
         vo.setChannelDesc(desc);
         Member member = new Member();
--- src/main/java/com/kh/youtube/controller/VideoController.java
+++ src/main/java/com/kh/youtube/controller/VideoController.java
@@ -7,6 +7,10 @@ import com.kh.youtube.service.VideoService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,13 +44,27 @@ public class VideoController {
 
     // 영상 전체 조회 : GET - http://localhost:8080/api/video
     @GetMapping("/video")
-    public ResponseEntity<List<Video>> VideoList() {
-        return ResponseEntity.status(HttpStatus.OK).body(videoService.showAll());
+    public ResponseEntity<List<Video>> VideoList(@RequestParam(name="page", defaultValue = "1") int page) {
+        //정렬
+        Sort sort = Sort.by("videoCode").descending();
+        // 한 페이지의 10개
+        Pageable pageable = PageRequest.of(page-1, 10, sort);
+        Page<Video> result = videoService.showAll(pageable);
+
+        log.info("Total Pages : " + result.getTotalPages());// 총 몇 페이지
+        log.info("Total Count : " + result.getTotalElements()); // 전체 개수
+        log.info("Page Number : " + result.getNumber()); // 현재 페이지 번호
+        log.info("Page Size : " + result.getSize()); // 페이지 당 데이터 개수
+        log.info("Next Page : " + result.hasNext()); // 다음 페이지가 있는지 존재 여부
+        log.info("First Page : " + result.isFirst()); // 시작 페이지 여부
+
+//        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.OK).body(result.getContent());
     }
 
     // 영상 추가 : POST - http://localhost:8080/api/video
     @PostMapping("/video")
-    public ResponseEntity<Video> createVideo(MultipartFile video, MultipartFile image, String title, String desc, String categoryCode) {
+    public ResponseEntity<Video> createVideo(MultipartFile video, MultipartFile image, String title, @RequestParam(name="desc", required = false) String desc, String categoryCode) {
         log.info("video : " + video);
         log.info("image : " + image);
         log.info("title : " + title);
@@ -87,13 +105,13 @@ public class VideoController {
         Video vo = new Video();
         vo.setVideoTitle(title);
         vo.setVideoDesc(desc);
-        vo.setVideoUrl(saveVideo);
-        vo.setVideoPhoto(saveImage);
+        vo.setVideoUrl(uuid + "_" + realVideo);
+        vo.setVideoPhoto(uuid + "_" + realImage);
         Category category = new Category();
         category.setCategoryCode(Integer.parseInt(categoryCode));
         vo.setCategory(category);
         Channel channel = new Channel();
-        channel.setChannelCode(21);
+        channel.setChannelCode(25);
         vo.setChannel(channel);
         Member member = new Member();
         member.setId("user1");
